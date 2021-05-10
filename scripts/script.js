74c6b014dfb6fa3c1e6bc0ed984880f732cc2917
Darian <d1hong@ucsd.edu>
2021-05-09 19:04:58 -0700

    Test run

--- scripts/script.js
+++ scripts/script.js
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
         newPost = document.createElement('journal-entry');
         newPost.entry = entry;
 
-        let img = newPost.getElementsByClassName('entry-image');
+        let img = newPost.article.getElementsByClassName('entry-image');
         img.src = entry.image.src;
         img.alt = entry.image.alt;
 
