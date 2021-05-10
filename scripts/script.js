f09fe9561b7948e3a7a9d0fcc2fc367b6b6c984f
Darian <d1hong@ucsd.edu>
2021-05-09 20:36:14 -0700

    Test run

--- scripts/script.js
+++ scripts/script.js
@@ -34,12 +34,12 @@ document.addEventListener('DOMContentLoaded', () => {
 
         let shadowDom = newPost.shadowRoot;
 
-        let img = document.createElement('img');
+        /*let img = document.createElement('img');
         img.className = 'entry-image';
         img.src = entry.img.src;
         img.alt = entry.img.alt;
 
-        shadowDom.appendChild(img);
+        shadowDom.appendChild(img);*/
 
         /*
         let article = shadowDom.querySelector("type=['article']");
